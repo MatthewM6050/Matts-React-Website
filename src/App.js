b83bc60a6774f173db011a6f6251ddba7cc7e0f8
Matthew Melendez <32375336+MatthewM6050@users.noreply.github.com>
2023-08-08 21:10:12 -0400

    Fixed homepage

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Route path="/about" exact element={<About/>} />
             <Route path='/contact' exact element={<Contact/>} />
           </Routes>
+          <Home/>
           <Footer/>
         </Router>
     </div>
